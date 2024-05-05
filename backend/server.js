1c687ab08ae698250bbc8306220705d647256199
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-05-05 14:43:15 +0530

    Change the Port and Setup Nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const db = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 
